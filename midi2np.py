81950f802a787c1479ea578be55493fa613a610e
KittyKittyKitKat <simon.palmer03@gmail.com>
2022-10-16 01:05:06 -0400

    Moved code to function

--- midi2np.py
+++ midi2np.py
@@ -1,9 +1,10 @@
 import mido
 import numpy as np
-mid = mido.MidiFile('Never-Gonna-Give-You-Up-3.mid', clip=True)
-merged = mido.merge_tracks(mid.tracks)
-messages = list(m.note for m in filter(lambda msg: msg.type=='note_on', merged))
-notes = np.empty(len(messages), dtype=object)
-notes[:] = messages
-# print(notes.tolist())
-# print(len(notes))
+
+def midi_to_np_arr(path_to_mid):
+    mid = mido.MidiFile('Never-Gonna-Give-You-Up-3.mid', clip=True)
+    merged = mido.merge_tracks(mid.tracks)
+    messages = list(m.note for m in filter(lambda msg: msg.type=='note_on', merged))
+    notes = np.empty(len(messages), dtype=object)
+    notes[:] = messages
+    return notes
